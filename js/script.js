9f0fdfc2d7dfc31b619d91ff4cb4f393d86a301e
cw developer <162027300+cwdeveloping@users.noreply.github.com>
2024-08-14 18:51:02 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,3 +2,8 @@ function togglePopup() {
     const popup = document.getElementById('popup');
     popup.classList.toggle('show');
 }
+window.addEventListener('click', (event) => {
+    if (event.target === popup) {
+        popup.classList.remove('show');
+    }
+});
